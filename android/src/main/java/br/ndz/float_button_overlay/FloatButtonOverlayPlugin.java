7d60b89d09b75127ae353794f91f0bfd4bd99c74
Nilton Diniz <niltondiniz@niltondiniz.com>
2021-10-22 17:05:54 -0300

    Corrigindo bug no android S

--- android/src/main/java/br/ndz/float_button_overlay/FloatButtonOverlayPlugin.java
+++ android/src/main/java/br/ndz/float_button_overlay/FloatButtonOverlayPlugin.java
@@ -124,7 +124,7 @@ public class FloatButtonOverlayPlugin extends Activity implements MethodCallHand
             Intent intent = mContext.getPackageManager().getLaunchIntentForPackage(packageName);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             PendingIntent pendingIntent =
-                    PendingIntent.getActivity(mContext, 0, intent, 0);
+                    PendingIntent.getActivity(mContext, 0, intent, 0, PendingIntent.FLAG_IMMUTABLE);
             try {
                 pendingIntent.send();
                 result.success(true);
